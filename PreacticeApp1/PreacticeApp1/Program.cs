7ba5845e0fd5c97322668d14967aa8c28ead8641
pial <pial0007l@gmail.com>
2023-03-06 01:14:31 +1030

    sending data with response

--- PreacticeApp1/PreacticeApp1/Program.cs
+++ PreacticeApp1/PreacticeApp1/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
+app.Run(async (HttpContext context) =>
+{
+    context.Response.Headers["TestKey"] = "Test 111";
+    context.Response.Headers["Server"] = "My Server";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("World");
+});
 app.Run();
